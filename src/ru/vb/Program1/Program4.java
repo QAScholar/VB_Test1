0ef5266ab7c413840b6eaf7b89072adc3397cc58
QAScholar <qa.scholar@yandex.ru>
2017-10-28 13:36:54 +1000

    4

---
+++ src/ru/vb/Program1/Program4.java
@@ -0,0 +1,18 @@
+package ru.vb.Program1;
+import java.util.Scanner;
+/**
+ * Created by user on 28.10.2017.
+ */
+public class Program4 {
+    public static void main(String[] args) {
+        Scanner a=new Scanner(System.in);
+        System.out.print("Enter n=");
+        int n=a.nextInt();
+        System.out.print("Enter b=");
+        int b=a.nextInt();
+        System.out.print("Enter c=");
+        int c=a.nextInt();
+        int d=(b+b+c)/3;
+        System.out.println("Среднее арифметическое чисел "+n+","+b+" и "+c+"= "+d);
+    }
+}
