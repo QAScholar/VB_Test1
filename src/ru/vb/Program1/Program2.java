568ca659a4419f7e5c48582479715863f4d5dc6e
QAScholar <qa.scholar@yandex.ru>
2017-10-28 12:14:38 +1000

    aa

--- src/ru/vb/Program1/Program2.java
+++ src/ru/vb/Program1/Program2.java
@@ -1,7 +1,14 @@
 package ru.vb.Program1;
-
+import java.util.Scanner;
 /**
  * Created by user on 28.10.2017.
  */
 public class Program2 {
+    public static void main(String[] args) {
+
+        Scanner a=new Scanner(System.in);
+        int x=a.nextInt();
+        int y=x%12;
+        System.out.println(y);
+    }
 }
