ea33850c9be7bbf1cbd4c8d377eb8ff32dac1619
QAScholar <qa.scholar@yandex.ru>
2017-10-28 13:51:46 +1000

    4

---
+++ src/ru/vb/Program1/Program6.java
@@ -0,0 +1,16 @@
+package ru.vb.Program1;
+import java.util.Scanner;
+/**
+ * Created by user on 28.10.2017.
+ */
+public class Program6 {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        System.out.print("Введите число=");
+        int x=a.nextInt();
+        System.out.print("Введите степень=");
+        int y=a.nextInt();
+
+        System.out.print("Число "+x+" в степени "+y+"= "+Math.pow(x,y));
+    }
+}
