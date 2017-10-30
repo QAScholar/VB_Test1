0c7c319362dd4361a232c674da019e9e5969cac0
QAScholar <qa.scholar@yandex.ru>
2017-10-30 20:33:25 +1000

    3

---
+++ src/ru/vb/Program1/Program3.java
@@ -0,0 +1,15 @@
+package ru.vb.Program1;
+import java.util.Scanner;
+/**
+ * Created by user on 28.10.2017.
+ */
+public class Program3 {
+    public static void main(String[] args) {
+        Scanner a=new Scanner(System.in);
+        System.out.print("Enter n=");
+        int n=a.nextInt();
+        int x=n+2-n%2;
+
+        System.out.println("After n goes="+x);
+    }
+}
