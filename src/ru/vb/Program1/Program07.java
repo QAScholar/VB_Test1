9fd0db57aa44497d4e2022bddeed72841332fec4
QAScholar <qa.scholar@yandex.ru>
2017-11-01 20:32:12 +1000

    2

---
+++ src/ru/vb/Program1/Program07.java
@@ -0,0 +1,27 @@
+package ru.vb.Program1;
+import java.util.Scanner;
+
+/* Задача 7.	Написать программу, которая  предлагает пользователю ввести радиус окружности,
+ а затем считает площадь и длину этой окружности. Число Pi задать в программе как вещественную константу.
+  */
+public class Program07 {
+
+    //Задаем константу Пи
+    public static final double Pi=3.14;
+
+    public static void main(String[] args) {
+
+        //Вводим радиус
+        System.out.print("Введите радиус окружности: ");
+        Scanner a=new Scanner(System.in);
+        double x=a.nextInt();
+
+        //Считаем площадь окружности
+        double S=Pi*(Math.pow(x,2));
+        System.out.println("Площадь окружности= "+ S);
+
+        //Считаем длину окружности
+        double L=Pi*(2*x);
+        System.out.println("Длина окружности= "+ L);
+    }
+}
