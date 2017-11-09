87b56e498d899b865388694de2694038bf8450d7
QAScholar <qa.scholar@yandex.ru>
2017-11-09 20:58:58 +1000

    rectangle

---
+++ src/ru/vb/Shapes/Rectangle.java
@@ -0,0 +1,22 @@
+package ru.vb.Shapes;
+
+/**
+ * Created by user on 09.11.2017.
+ */
+public class Rectangle  {
+    int a,b,x,y;
+    String color;
+
+    public Rectangle (){};
+
+    public Rectangle (int a, int b){};
+
+    public Rectangle (int a, int b, int x, int y){};
+    
+    public int square(){
+        return (this.a*this.b);
+    }
+    int area(){
+
+    }
+}
