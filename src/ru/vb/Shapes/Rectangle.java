27059e84885c05110b4a9cc02c56077240e0f8e2
QAScholar <qa.scholar@yandex.ru>
2017-11-09 21:08:06 +1000

    rectangle

--- src/ru/vb/Shapes/Rectangle.java
+++ src/ru/vb/Shapes/Rectangle.java
@@ -7,16 +7,29 @@ public class Rectangle  {
     int a,b,x,y;
     String color;
 
+    public static void main(String[] args) {
+        Rectangle rect1=new Rectangle();
+    }
+
+
+
     public Rectangle (){};
 
     public Rectangle (int a, int b){};
 
     public Rectangle (int a, int b, int x, int y){};
-    
+
     public int square(){
         return (this.a*this.b);
     }
     int area(){
+        return (2*this.a+2*this.b);
+    }
+    String getColor(){
+        return color;
+    }
+        void getColor(String color){
+        this.color=color;
 
     }
 }
